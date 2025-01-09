1d3da7dedcf80aa7ac75365f90426414635f9518
Миша Зуев <mzuew2006@gmail.com>
2025-01-09 09:48:17 +0100

    Added functionality for creating, displaying and deleting tasks with notifications

--- js/script.js
+++ js/script.js
@@ -1,65 +1,184 @@
-const submitBtn = document.querySelector("#submit_button"),
-  taskTitle = document.querySelector("#taskTitle"),
-  taskDescription = document.querySelector("#taskDescription"),
-  notification = document.querySelector("#responseMessage"),
-  form = document.querySelector("#taskForm");
+window.addEventListener("DOMContentLoaded", () => {
+  const submitBtn = document.querySelector("#submit_button"),
+    taskTitle = document.querySelector("#taskTitle"),
+    taskDescription = document.querySelector("#taskDescription"),
+    notification = document.querySelector("#responseMessage"),
+    form = document.querySelector("#taskForm"),
+    taskList = document.querySelector(".task-list");
 
-const message = {
-  success: "Успешно",
-  loading: "Загрузка...",
-  failure: "Что-то пошло не так...",
-};
+  const message = {
+    success: "Успешно",
+    loading: "Загрузка...",
+    failure: "Что-то пошло не так...",
+  };
 
-submitBtn.addEventListener("click", async (event) => {
-  event.preventDefault();
+  loadTasks();
 
-  const titileOfTask = taskTitle.value,
-    descriptionOfTask = taskDescription.value;
+  // Submiting data to server
 
-  if (!titileOfTask || !descriptionOfTask) {
-    showNotification("Заполните все поля", 2500);
+  submitBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+    console.log("Форма отправляется...");
 
-    return;
-  } else {
-    const formData = new FormData(form);
+    const titileOfTask = taskTitle.value,
+      descriptionOfTask = taskDescription.value;
 
-    const json = JSON.stringify(Object.fromEntries(formData.entries()));
+    if (!titileOfTask || !descriptionOfTask) {
+      showNotification("Заполните все поля", 3000);
+
+      return;
+    } else {
+      const formData = new FormData(form);
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+      try {
+        const res = await createTask("http://localhost:3000/todos", json);
+
+        showNotification(message.success, 2000);
+        console.log("Задача добавлена:", res);
+
+        loadTasks();
+      } catch (error) {
+        showNotification(message.failure, 3000);
+        console.error(error);
+      } finally {
+        form.reset();
+      }
+    }
+  });
+
+  function showNotification(message, time = 500) {
+    notification.textContent = `${message}`;
+    notification.classList.add("show");
+    notification.classList.remove("hide");
+
+    setTimeout(() => {
+      notification.classList.add("hide");
+      notification.classList.remove("show");
+    }, time);
+  }
+
+  async function createTask(url, data) {
     try {
-      const res = await createTask("http://localhost:3000/todos", json);
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: data,
+      });
 
-      showNotification(message.success, 1000);
-      console.log("Задача добавлена:", res);
+      return await response.json();
     } catch (error) {
-      showNotification(message.failure, 2500);
-    } finally {
-      form.reset();
+      console.error(error);
     }
   }
-});
 
-function showNotification(message, time = 500) {
-  notification.textContent = `${message}`;
-  notification.classList.add("show");
-  notification.classList.remove("hide");
+  // Class for list of tasks
 
-  setTimeout(() => {
-    notification.classList.add("hide");
-    notification.classList.remove("show");
-  }, time);
-}
+  class TaskCard {
+    constructor(title, description) {
+      this.title = title;
+      this.description = description;
+    }
 
-async function createTask(url, data) {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: data,
-    });
+    render(taskNumber, taskId) {
+      const li = document.createElement("li");
+      li.classList.add("task-item");
 
-    return await response.json();
-  } catch (error) {
-    console.error(error);
+      li.innerHTML = `
+        <span class="task-number">${taskNumber}.</span>
+        <span class="task-title">${this.title}</span>
+        <span class="task-description">${this.description}</span>
+        <div class="task-actions">
+          <button class="complete-btn">Выполнено</button>
+          <button class="change-btn">Изменить</button>
+          <button class="delete-btn" data-id="${taskId}">Удалить</button>
+        </div>
+      `;
+      return li;
+    }
   }
-}
+
+  async function loadTasks() {
+    try {
+      const tasks = await getTasks("http://localhost:3000/todos");
+      console.log("Полученные задачи:", tasks);
+
+      if (tasks.length > 0) {
+        taskList.innerHTML = "";
+        tasks.forEach(({ id, title, description }, index) => {
+          const newTask = new TaskCard(title, description).render(
+            index + 1,
+            id
+          );
+          taskList.appendChild(newTask);
+        });
+      } else {
+        console.error("Список задач пуст");
+      }
+    } catch (error) {
+      console.error("Ошибка загрузки задач:", error);
+    }
+  }
+
+  async function getTasks(url) {
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Ошибка загрузки задач:", error);
+      throw error;
+    }
+  }
+
+  // Task actions
+
+  taskList.addEventListener("click", (event) => {
+    if (event.target.matches(".complete-btn")) {
+      const parent = event.target.closest(".task-item");
+
+      if (
+        event.target.matches(".complete-btn") &&
+        event.target.matches(".strike-through")
+      ) {
+        event.target.classList.remove("strike-through");
+        event.target.textContent = "Выполнено";
+
+        parent.style.backgroundColor = "#fff";
+      } else {
+        event.target.classList.add("strike-through");
+        event.target.textContent = "Отмена";
+
+        parent.style.backgroundColor = "#2ee95a";
+      }
+    } else if (event.target.matches(".delete-btn")) {
+      const taskId = event.target.dataset.id;
+
+      deleteTask(taskId);
+    }
+  });
+
+  function deleteTask(num) {
+    fetch(`http://localhost:3000/todos/${num}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Задача удалена успешно");
+
+          loadTasks();
+        } else {
+          console.log("Произошла ошибка", response.status);
+        }
+      })
+      .catch((error) => {
+        console.error("Ошибка запроса:", error);
+      });
+  }
+});
